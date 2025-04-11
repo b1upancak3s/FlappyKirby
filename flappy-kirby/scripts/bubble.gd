9b34f9d44db7f010d0bc1209f0d987b89aa9c064
blu <aidanmui809@gmail.com>
2025-04-11 11:08:56 -0400

    Delete flappy-kirby directory
    outdated version

--- flappy-kirby/scripts/bubble.gd
+++
@@ -1,6 +0,0 @@
-extends Area2D
-
-signal bubblehit
-
-func _on_body_entered(body: Node2D) -> void:
-	bubblehit.emit()
--- flappy-kirby/scripts/pipes.gd
+++
@@ -1,8 +0,0 @@
-extends Area2D
-
-signal pipehit
-
-signal pipescore
-
-func _on_body_entered(body: Node2D):
-	pipehit.emit()
